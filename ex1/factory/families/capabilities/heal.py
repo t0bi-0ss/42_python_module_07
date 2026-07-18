1fd797b4ed052caf17099aef7960ed03a3c7fa4b
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-07-19 00:59:45 +0200

    Define HealCapability abstract class

---
+++ ex1/factory/families/capabilities/heal.py
@@ -0,0 +1,18 @@
+"""Holds HealCapability abstract class"""
+
+from abc import ABC, abstractmethod
+
+from typing import Protocol
+
+
+class Creature(Protocol):
+    """Protocol class for any creature"""
+    _name: str
+    _type: str
+
+
+class HealCapability(ABC):
+
+    @abstractmethod
+    def heal(target: Creature) -> str:
+        pass
