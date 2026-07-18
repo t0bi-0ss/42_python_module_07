ea47f99838e87d61dd8733890de2abd3d498f885
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-07-19 01:57:24 +0200

    Define 'Shiftling' creature family classes

---
+++ ex1/factory/families/shiftling.py
@@ -0,0 +1,53 @@
+"""Holds all classes for the 'Shiftling' creature family"""
+
+from .abstract_creature import Creature
+
+from .capabilities.transform import TransformCapability
+
+
+class Shiftling(Creature, TransformCapability):
+    """Shiftling Transformable Creature class"""
+
+    def __init__(self) -> None:
+        Creature.__init__(self)
+        TransformCapability.__init__(self)
+        self._name: str = "Shiftling"
+        self._type: str = "Normal"
+
+    def transform(self) -> None:
+        self._is_transformed = True
+        return f"{self._name} shifts into a sharper form!"
+
+    def revert(self) -> None:
+        self._is_transformed = False
+        return f"{self._name} returns to normal"
+
+    def attack(self) -> str:
+        if not self._is_transformed:
+            return f"{self._name} attacks normally"
+        else:
+            return f"{self._name} performs a boosted strike"
+
+
+class Morphagon(Creature, TransformCapability):
+    """Morphagon Transformable Creature class"""
+
+    def __init__(self) -> None:
+        Creature.__init__(self)
+        TransformCapability.__init__(self)
+        self._name = "Morphagon"
+        self._type = "Normal/Dragon"
+
+    def transform(self) -> None:
+        self._is_transformed = True
+        return f"{self._name} morphs into a dragonic battle form!"
+
+    def revert(self) -> None:
+        self._is_transformed = False
+        return f"{self._name} stabilizes its form"
+
+    def attack(self) -> None:
+        if self._is_transformed:
+            return f"{self._name} unleashes a devastating morph strike!"
+        else:
+            return f"{self._name} attacks normally"
