4d872b8fabdfccfd11a7c34184bd91da9223a0c6
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-07-18 22:43:37 +0200

    Modify Flameling creature family constructors

--- ex0/factory/families/flameling.py
+++ ex0/factory/families/flameling.py
@@ -5,11 +5,19 @@ from .abstract_creature import Creature
 
 class Flameling(Creature):
 
+    def __init__(self) -> None:
+        self._name = "Flameling"
+        self._type = "Fire"
+
     def attack(self) -> str:
         return f"{self._name} uses Ember"
 
 
 class Pyrodon(Creature):
+
+    def __init__(self) -> None:
+        self._name = "Pyrodon"
+        self._type = "Fire/Flying"
 
     def attack(self) -> str:
         return f"{self._name} uses Flamethrower"
