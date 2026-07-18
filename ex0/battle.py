b85a270086b214b2927e612dd2b816fcdf419082
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-07-19 00:20:23 +0200

    Write required test script

---
+++ ex0/battle.py
@@ -0,0 +1,48 @@
+from factory import AquaFactory, FlameFactory, CreatureFactory
+
+
+"""Test ex0 package"""
+
+# Instatiate factories
+flame_factory = FlameFactory()
+aqua_factory = AquaFactory()
+
+
+def factory_test(factory: CreatureFactory) -> None:
+    """Verifies if factory can create base and evolved Creature"""
+
+    print("Testing factory")
+    base_form = factory.create_base()
+    evolved_form = factory.create_evolved()
+
+    print(base_form.describe())
+    print(base_form.attack())
+    print(evolved_form.describe())
+    print(evolved_form.attack())
+
+
+factory_test(flame_factory)
+print()
+factory_test(aqua_factory)
+
+print()
+
+
+def battle_test(
+        factory_1: CreatureFactory, factory_2: CreatureFactory
+        ) -> None:
+    """Battle test between creatures created by two different factories"""
+
+    print("Testing battle")
+    creature_1 = factory_1.create_base()
+    creature_2 = factory_2.create_base()
+
+    print(creature_1.describe())
+    print(" vs.")
+    print(creature_2.describe())
+    print(" fight!")
+    print(creature_1.attack())
+    print(creature_2.attack())
+
+
+battle_test(flame_factory, aqua_factory)
