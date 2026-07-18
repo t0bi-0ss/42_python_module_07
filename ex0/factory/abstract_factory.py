46071f7c229059e5366c4a48cea0ebf68d9827b4
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-07-18 23:44:09 +0200

    Define CreatureFactory abstract class

---
+++ ex0/factory/abstract_factory.py
@@ -0,0 +1,18 @@
+"""Holds abstract class for every type of Factory"""
+
+from abc import ABC, abstractmethod
+from families.abstract_creature import Creature
+
+
+class CreatureFactory(ABC):
+    """Abstract class for every type of Factory"""
+
+    @abstractmethod
+    def create_base(self) -> Creature:
+        """Creates creature base form"""
+        pass
+
+    @abstractmethod
+    def create_evolved(self) -> Creature:
+        """Creates creature evolved form"""
+        pass
