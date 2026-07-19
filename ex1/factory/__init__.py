4aff30cad1eb20538af8bb98dfdaaa80038ecae3
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-07-19 02:06:30 +0200

    Make partial imports for new factories

--- ex1/factory/__init__.py
+++ ex1/factory/__init__.py
@@ -1,9 +1,13 @@
 from .aqua_factory import AquaFactory
 from .flame_factory import FlameFactory
 from .abstract_factory import CreatureFactory
+from .healing_factory import HealingCreatureFactory
+from .transform_factory import TransformCreatureFactory
 
 __all__ = [
     "CreatureFactory",
     "AquaFactory",
-    "FlameFactory"
+    "FlameFactory",
+    "HealingCreatureFactory",
+    "TransformCreatureFactory"
 ]
