8142db37ef19eb112922509a7ebab8d99671bbd7
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-07-19 02:06:09 +0200

    Define TransformCreatureFactory CreatureFactory class

---
+++ ex1/factory/transform_factory.py
@@ -0,0 +1,15 @@
+"""Holds TransformCreatureFactory class"""
+
+from .abstract_factory import CreatureFactory
+
+from .families import shiftling
+
+
+class TransformCreatureFactory(CreatureFactory):
+    """TransformCreatureFactory class"""
+
+    def create_base(self) -> shiftling.Shiftling:
+        return shiftling.Shiftling()
+
+    def create_evolved(self) -> shiftling.Morphagon:
+        return shiftling.Morphagon()
